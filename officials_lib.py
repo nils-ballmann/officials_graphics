7abeae266e315a021171a7f68671a48c8f2e3ced
Nils Ballmann <nils.ballmann@gmail.com>
2019-01-28 12:25:22 +0100

    started a lib, wanting to build a complete field, with correct measurements

---
+++ officials_lib.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+# pylint: disable=C0111
+
+__updated__ = '2019-01-28 12:24:29'
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.collections as mcollections
